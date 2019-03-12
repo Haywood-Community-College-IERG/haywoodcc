a3df1eef8b13d6e7cef55a0bf9b02c81fbee1023
David Onder <donder@haywood.edu>
2019-03-12 11:35:08 -0400

    Add Early College and all high school information to data.

--- R/data.r
+++ R/data.r
@@ -1,9 +1,40 @@
-#' All current CCP program codes.
+#' All current Career and College Promise (CCP) program codes.
 #'
-#' A dataset containing the program codes of all active CCP programs.
+#' A dataset containing the program codes of all active and previous Career and College Promise (CCP) programs.
 #'
 #' @format A data frame with 38 rows and 1 variable:
 #' \describe{
 #'   \item{Program}{Program code}
 #' }
 "ccp_programs"
+
+#' All current Early College (EC) program codes.
+#'
+#' A dataset containing the program codes of all active and previous Early College (EC) programs.
+#'
+#' @format A data frame with 3 rows and 1 variable:
+#' \describe{
+#'   \item{Program}{Program code}
+#' }
+"early_college_programs"
+
+#' All current high school program codes.
+#'
+#' A dataset containing the program codes of all active and previous high school programs.
+#'
+#' @format A data frame with 41 rows and 1 variable:
+#' \describe{
+#'   \item{Program}{Program code}
+#' }
+"high_school_programs"
+
+#' All current high schools and codes for Haywood County.
+#'
+#' A dataset containing the high school names and codes for Haywood County.
+#'
+#' @format A data frame with 4 rows and 2 variable:
+#' \describe{
+#'   \item{School}{School Name}
+#'   \item{Institution_ID}{The Colleague institution ID}
+#' }
+"haywood_county_high_schools"
---
+++ data-raw/high_school_programs.r
@@ -0,0 +1,31 @@
+
+ccp_programs <- data.frame( Program = c( "C15200CP", "C150200CP",
+                                         "C2506CP",  "C250200TP",
+                                         "C25100CP", "C25120BE", "C25310CP", "C25340CP", "C25550CB", "C25590CB",
+                                         "C35130CP", "C35140CP", "C35140CB",
+                                         "C40200CP", "C40200CD",
+                                         "C50210CP", "C50210AD", "C50420CP",
+                                         "C55180CP", "C55180II", "C55180IV", "C55220CP", "C55400CP", "C55860CP", "C55960CP",
+                                         "C60130IN", "C60130WE", "C60130SP", "C60160CP", "C60160ES", "C60160IN", "C60160IM",
+                                         "P1012A", "P1012B", "P1012C", "P1042A", "P1042B", "P1042C" ),
+                            stringsAsFactors = FALSE )
+
+early_college_programs <- data.frame( Program = c( "A10100EC","T90970EC","T90930" ), stringsAsFactors = FALSE )
+
+high_school_programs <- dplyr::bind_rows(ccp_programs, early_college_programs)
+
+haywood_county_high_schools <- data.frame( School = c( "Tuscola", "Pisgah", "Central Haywood", "Haywood Early College" ),
+                                           Institution_ID = c( "0034905", "0034337", "0034402", "1113888"),
+                                           stringsAsFactors = FALSE )
+
+readr::write_csv(ccp_programs, "data-raw/ccp_programs.csv")
+usethis::use_data(ccp_programs, overwrite = TRUE, compress = 'xz')
+
+readr::write_csv(early_college_programs, "data-raw/early_college_programs.csv")
+usethis::use_data(early_college_programs, overwrite = TRUE, compress = 'xz')
+
+readr::write_csv(high_school_programs, "data-raw/high_school_programs.csv")
+usethis::use_data(high_school_programs, overwrite = TRUE, compress = 'xz')
+
+readr::write_csv(haywood_county_high_schools, "data-raw/haywood_county_high_schools.csv")
+usethis::use_data(haywood_county_high_schools, overwrite = TRUE, compress = 'xz')
