3d7e98d00b9b8ce00014b88a60570ae1742f416a
David Onder <donder@haywood.edu>
2022-01-21 17:01:48 -0500

    Added getCfg function to load the configuration file. Configuration is cached for subsequent calls.

--- R/informer.r
+++ R/informer.r
@@ -1,5 +1,43 @@
 pkg.env <- new.env(parent = emptyenv())
 
+pkg.env$cfg = NA_character_
+
+#' Return a data from data warehouse Colleague tables.
+#'
+#' Return a data from of the IPEDS cohort data. Data will come either from the file ipeds_cohorts.csv of
+#' from the CCDW_HIST SQL Server database.
+#'
+#' @param cfg_fn The file name for the YAML configuration file. Defaults to config.yml.
+#' @param cfg_path The file path to the YAML configuration file. Defaults to ".".
+#' @param reload Force a reload of the config.
+#' @export
+#'
+getCfg <- function( cfg_fn="config.yml", cfg_path=".", reload=FALSE ) {
+
+    cfg <- pkg.env$cfg
+    if (typeof(pkg.env$cfg) == "character" || reload) {
+        cfg_full_path <- fs::path(cfg_path,cfg_fn)
+
+        if (fs::file_exists(cfg_full_path)) {
+            cfg_l <- yaml::yaml.load_file(cfg_full_path)
+            if (cfg_l$config$location == "self") {
+                cfg = cfg_l
+                pkg.env$cfg_full_path <- cfg_full_path
+            } else {
+                cfg_full_path <- fs::path(cfg_l$config$location,cfg_fn)
+
+                if (fs::file_exists(cfg_full_path)) {
+                    cfg <- yaml::yaml.load_file(cfg_full_path)
+                    pkg.env$cfg_full_path <- cfg_full_path
+                }
+            }
+
+            pkg.env$cfg <- cfg
+        }
+    }
+    getCfg <- cfg
+}
+
 #' Return a data from data warehouse Colleague tables.
 #'
 #' Return a data from of the IPEDS cohort data. Data will come either from the file ipeds_cohorts.csv of
@@ -8,16 +46,16 @@ pkg.env <- new.env(parent = emptyenv())
 #' @param file The name of the Colleague file to return
 #' @param schema Which schema should be used. Needed for non-Colleague tables.
 #' @param version Specify which version to include. Default is for the latest data. Any other value will return the dated file.
-#' @param cfg A YAML configuration file with sql section that includes driver, server, db, and schema_history
+#' @param cfg A YAML configuration file with sql section that includes driver, server, db, and schema_history.
+#' @param cfg_fn The file name for the YAML configuration file. Defaults to config.yml.
+#' @param cfg_path The file path to the YAML configuration file. Defaults to ".".
 #' @param sep The separator to use in the field names. Default is a '.' as in the original Colleague file.
 #' @export
 #' @importFrom stringr str_c
 #'
-getColleagueData <- function( file, schema="history", version="latest", cfg=NA_character, sep='.' ) {
+getColleagueData <- function( file, schema="history", version="latest", sep='.', cfg=NA_character, cfg_fn="config.yml", cfg_path="." ) {
 
-    if (is.na(cfg)) {
-        cfg <- yaml::yaml.load_file("config.yml")
-    }
+    cfg <- getCfg(cfg_fn=cfg_fn, cfg_path=cfg_path)
 
     conn_str <- str_c( str_c("Driver",   str_c("{", cfg$sql$driver, "}"), sep="="),
                        str_c("Server",   cfg$sql$server,                  sep="="),
